618fb6255a5b2badfadd9027e7f0c72dc43b8d4d
Chen Pan <pan0xc@foxmail.com>
2026-07-01 22:40:55 +0800

    update

--- flake.nix
+++ flake.nix
@@ -5,19 +5,21 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
 
-    pkgs = import nixpkgs {
-      inherit system;
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs_24
+          pnpm
+        ];
+      };
     };
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        nodejs_24
-        pnpm
-      ];
-    };
-  };
 }
