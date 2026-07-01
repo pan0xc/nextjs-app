ae3186b574e1e15c462792761331f4a42a6966db
Chen Pan <pan0xc@foxmail.com>
2026-07-01 22:54:21 +0800

    init commit

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  // typedRoutes: true,
-  experimental: {
-    turbopackFileSystemCacheForDev: false,
-  },
-  cacheComponents: true,
 };
 
 export default nextConfig;
--- prisma/migrations/20260615111833_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Todo" (
-    "id" SERIAL NOT NULL,
-    "text" TEXT NOT NULL,
-    "completed" BOOLEAN NOT NULL,
-
-    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
-);
